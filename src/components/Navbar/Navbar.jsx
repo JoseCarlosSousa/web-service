510d1e62ad501f7ebca2aab7522a6ed99ddd64ce
Carlos Sousa <kkosmico@gmail.com>
2026-08-31 21:02:16 +0100

    dd

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useTranslation } from "react-i18next";
-import { AVAILABLE_LANGUAGES } from "../constants/countries";
+import { AVAILABLE_LANGUAGES } from "../../constants/countries";
 import "./Navbar.css";
 
 export default function Navbar({ isLoggedIn, onLogout }) {
